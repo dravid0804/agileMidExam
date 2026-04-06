afa15259e4c4f6ec09b44c9e2501799ff6e80b26
Dravid Anand B <dravidbabu11@gmail.com>
2026-04-06 12:52:11 +0530

    Add files via upload

--- main/java/com/example/App.java
+++ main/java/com/example/App.java
@@ -1,22 +1,28 @@
+package com.example;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // Create Scanner object for dynamic input
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("Enter a non-negative integer: ");
-        int num = sc.nextInt();
-        
-        if (num < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            long factorial = 1;
-            for (int i = 1; i <= num; i++) {
-                factorial *= i;
+        if (sc.hasNextInt()) {
+            int num = sc.nextInt();
+            try {
+                System.out.println("Factorial of " + num + " is: " + calculateFactorial(num));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-            System.out.println("Factorial of " + num + " is: " + factorial);
         }
-        sc.close(); // Best practice to close the scanner
+        sc.close();
+    }
+
+    public static long calculateFactorial(int num) {
+        if (num < 0) throw new IllegalArgumentException("Factorial not defined for negative numbers.");
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
 }
